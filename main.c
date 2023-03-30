fb6f7c58ec17d0fd321b4a41dc34cb4ec4eda626
NickolAir <nikolay.ratushnyak03@gmail.com>
2023-03-30 16:57:57 +0700

    set grid

--- main.c
+++ main.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <math.h>
 
 #define DEFAULT 3
 
-double *create_matrix (int N){
-    double *A = (double*) malloc(N * N * sizeof(double));
-    for (int i = 0; i < N * N; ++i) {
+int numprocs = 0;
+int rank = 0;
+int gridSize;
+int gridCoords[2];
+MPI_Comm gridComm;
+MPI_Comm colComm;
+MPI_Comm rowComm;
+
+double *create_matrix (int N, int M){
+    double *A = (double*) malloc(N * M * sizeof(double));
+    for (int i = 0; i < N * M; ++i) {
         A[i] = 1.0;
     }
     return A;
 }
 
-double *create_emptyMatrix (int N){
-    double *A = (double*) malloc(N * N * sizeof(double));
-    for (int i = 0; i < N * N; ++i) {
+double *create_emptyMatrix (int N, int M){
+    double *A = (double*) malloc(N * M * sizeof(double));
+    for (int i = 0; i < N * M; ++i) {
         A[i] = 0.0;
     }
     return A;
 }
 
-void print_matrix(double *Matrix, int N) {
-    for (int i = 0; i < N * N; ++i) {
+void print_matrix(double *Matrix, int N, int M) {
+    for (int i = 0; i < N * M; ++i) {
         printf("%f ", Matrix[i]);
         if (i != 0 && (i + 1) % N == 0) {
             printf("\n");
@@ -29,12 +38,14 @@ void print_matrix(double *Matrix, int N) {
     }
 }
 
-void matrix_multiplication(double *A, double *B, double *Res, int size) {
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            for (int k = 0; k < size; ++k) {
-                Res[i * size + j] += A[i * size + k] * B[k * size + j];
+void matrix_multiplication(double *A, double *B, double *Res, int N1, int N2, int N3) {
+    for (int i = 0; i < N1; i++) {
+        for (int j = 0; j < N3; j++) {
+            int sum = 0;
+            for (int k = 0; k < N2; k++) {
+                sum += A[i * N2 + k] * B[k * N3 + j];
             }
+            Res[i * N3 + j] = sum;
         }
     }
 }
@@ -45,24 +56,53 @@ void FreeProcess(double* A, double* B, double* Res) {
     free(Res);
 }
 
+void create_gridComm() {
+    int dims[2] = {gridSize, gridSize}, periods[2] = {1, 1}, subdims[2] = {0, 1};
+
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 1, &gridComm);
+
+    MPI_Cart_coords(gridComm, rank, 2, gridCoords);
+
+    MPI_Cart_sub(gridComm, subdims, &rowComm);
+
+    subdims[0] = 1;
+    subdims[1] = 0;
+    MPI_Cart_sub(gridComm, subdims, &colComm);
+}
+
 int main(int argc, char *argv[]) {
     double *MatrixA = NULL, *MatrixB = NULL, *MatrixRes = NULL;
     double start_time = 0.0, end_time;
-    int numprocs, rank;
-    int size = DEFAULT;
+    int N1 = DEFAULT, N2 = DEFAULT, N3 = DEFAULT;
     if (argc >= 2){
-        size = atoi(argv[1]);
+        N1 = atoi(argv[1]);
+        N2 = atoi(argv[2]);
+        N3 = atoi(argv[3]);
     }
-    MatrixA = create_matrix(size);
-    MatrixB = create_matrix(size);
-    MatrixRes = create_emptyMatrix(size);
-    matrix_multiplication(MatrixA, MatrixB, MatrixRes, size);
-    print_matrix(MatrixRes, size);
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Status status;
+
+    int gridSize = (int)sqrt((double)numprocs);
+    if (numprocs != gridSize * gridSize) {
+        if (rank == 0) {
+            printf ("Number of processes must be a perfect square \n");
+        }
+    }
+    else {
+        if (rank == 0) {
+            MatrixA = create_matrix(N1, N2);
+            MatrixB = create_matrix(N2, N3);
+            MatrixRes = create_emptyMatrix(N1, N3);
+
+            create_gridComm();
+
+            matrix_multiplication(MatrixA, MatrixB, MatrixRes, N1, N2, N3);
+            print_matrix(MatrixRes, N1, N3);
+        }
+    }
     
     MPI_Finalize();
     FreeProcess(MatrixA, MatrixB, MatrixRes);
