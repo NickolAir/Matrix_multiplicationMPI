5cba270b3d7b696a14135711877ac72c43617146
NickolAir <nikolay.ratushnyak03@gmail.com>
2023-04-06 19:25:28 +0700

    create communicators

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 #define max(X, Y) (((X) > (Y)) ? (X) : (Y))
 #define DEFAULT 3
+#define Y 0
+#define X 1
 
 int isPrime(int x) {
     for (int i = 2; i <= sqrt(x); ++i) {
@@ -65,7 +67,7 @@ void FreeProcess(double* A, double* B, double* Res, double* bA, double* bB, doub
 void create_gridComm(int *dims, int *periods, int *coords, int numprocs, MPI_Comm *gridComm, int rank) {
     MPI_Dims_create(numprocs, 2, dims);
     if (rank == 0)
-        printf("%d %d\n", dims[0], dims[1]);
+        printf("%d %d\n", dims[Y], dims[X]);
     //creating communicator of 2d grid
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, gridComm);
     //getting rank of process in grid
@@ -73,24 +75,38 @@ void create_gridComm(int *dims, int *periods, int *coords, int numprocs, MPI_Com
     //get coordinates in 2d grid
     //MPI_Cart_get(gridComm, 2, dims, periods, coords);
     MPI_Cart_coords(*gridComm, rank, 2, coords);
-    printf("rank: %d coords: %d %d\n", rank, coords[0], coords[1]);
+    printf("rank: %d coords: %d %d\n", rank, coords[Y], coords[X]);
 }
 
-void create_Comms(MPI_Comm *gridComm, MPI_Group *gridGroup, int *coords, MPI_Comm rowComms, MPI_Comm colComms, int *dims) {
+void create_Comms(MPI_Comm *gridComm, MPI_Group *gridGroup, MPI_Comm *rowComms, MPI_Comm *colComms, int *dims) {
     MPI_Group buffer;
     int cur_coord[2] = {0, 0};
     int rank;
     int *rankArray = (int*) malloc(dims[0] * sizeof(int));
-    for (int i = 0; i < dims[1]; ++i) {
-        for (int j = 0; j < dims[0]; ++j) {
-            cur_coord[1] = i;
-            cur_coord[0] = j;
+    for (int i = 0; i < dims[X]; ++i) {
+        for (int j = 0; j < dims[Y]; ++j) {
+            cur_coord[X] = i;
+            cur_coord[Y] = j;
             MPI_Cart_rank(*gridComm, cur_coord, &rank);
             rankArray[j] = rank;
-            printf("coords: %d %d\n", cur_coord[0], cur_coord[1]);
         }
-        MPI_Group_incl(*gridGroup, dims[0], rankArray, &buffer);
+        MPI_Group_incl(*gridGroup, dims[Y], rankArray, &buffer);
+        MPI_Comm_create(*gridComm, buffer, &colComms[i]);
     }
+
+    rankArray = realloc(rankArray, dims[1] * sizeof(int));
+    for (int i = 0; i < dims[Y]; ++i) {
+        for (int j = 0; j < dims[X]; ++j) {
+            cur_coord[Y] = i;
+            cur_coord[X] = j;
+            MPI_Cart_rank(*gridComm, cur_coord, &rank);
+            rankArray[j] = rank;
+        }
+        MPI_Group_incl(*gridGroup, dims[X], rankArray, &buffer);
+        MPI_Comm_create(*gridComm, buffer, &rowComms[i]);
+    }
+
+    free(rankArray);
 }
 
 /*void partition(double *Matrix, double *blockMatrix, int size, int blockSize) {
@@ -114,8 +130,6 @@ void data_distribution(double *A, double *B, double *Ablock, double *Bblock, int
 
 int main(int argc, char *argv[]) {
     MPI_Comm gridComm;
-    MPI_Comm colComm;
-    MPI_Comm rowComm;
     MPI_Group gridGroup;
     int dims[2] = {0, 0};
     int periods[2] = {0, 0};
@@ -137,7 +151,9 @@ int main(int argc, char *argv[]) {
     create_gridComm(dims, periods, coords, numprocs, &gridComm, rank);
     int rankX = coords[0], rankY = coords[1];
     MPI_Comm_group(gridComm, &gridGroup);
-    create_Comms(&gridComm, &gridGroup, coords, rowComm, colComm, dims);
+    MPI_Comm *colComm = (MPI_Comm*) malloc(dims[X] * sizeof(MPI_Comm));
+    MPI_Comm *rowComm = (MPI_Comm*) malloc(dims[Y] * sizeof(MPI_Comm));
+    create_Comms(&gridComm, &gridGroup, rowComm, colComm, dims);
 
 //    double *blockA = (double *) malloc(blockSizeA * sizeof(double));
 //    double *blockB = (double *) malloc(blockSizeB * sizeof(double));
